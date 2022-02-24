e55b4794362af35f900461b32498b8991f9fa745
Kibuchi-muchai <dickson.kibuchi@student.moringaschool.com>
2022-02-24 03:04:22 -0500

    includinf exported components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WquotesComponent } from './wquotes/wquotes.component';
+import { WquotesDetailComponent } from './wquotes-detail/wquotes-detail.component';
+import { WquotesFormComponent } from './wquotes-form/wquotes-form.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { UpwardComponent } from './upward/upward.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WquotesComponent,
+    WquotesDetailComponent,
+    WquotesFormComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    UpwardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

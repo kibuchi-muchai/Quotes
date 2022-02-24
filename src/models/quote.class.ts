177a47f9e3eb6db54e2cb8e018948272d7895ae7
Kibuchi-muchai <dickson.kibuchi@student.moringaschool.com>
2022-02-24 03:37:26 -0500

    changes

--- src/models/quote.class.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-    public quote:string;
-    public author:string;
-
-    constructor(quote: string, author: string){
-        this.quote = quote;
-        this.author = author;
-    }
-}
